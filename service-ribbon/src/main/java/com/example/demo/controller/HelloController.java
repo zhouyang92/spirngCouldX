1856a0081f257d9844138199844b7dd0cfa9dea1
zhouyang <809619320@qq.com>
2018-07-12 19:31:33 +0800

    上传springCould文件

---
+++ service-ribbon/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by admin on 2018/7/12.
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+    @RequestMapping
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+
+}
